8b9f2b103f478c3c64384a7ba8d1bed8cf997c3b
ahaydeid <adi.hadi270@gmail.com>
2026-01-06 08:32:58 +0700

    fix: disable force HTTPS in local development environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
    public function boot(): void
     {
         // Force HTTPS in production
-        if (config('app.env') === 'production' || config('app.env') === 'local') {
+        if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
 
